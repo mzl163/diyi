489471e947c36dc2c135231165349982b61f40a9
mzl163 <52455562+mzl163@users.noreply.github.com>
2019-07-03 22:31:35 +0800

    Add files via upload

---
+++ morse.ino
@@ -0,0 +1,24 @@
+char ch_hui = '?';
+
+String out = "";
+void setup() {
+  // put your setup code here, to run once:
+    Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+   string a[26]={
+".-**","-...","-.-.","-..*",".***","..-.","--.*","....","..**",".---","-.-*",".-..","--**","-.**","---*",".--.","--.-",".-.*","...*","-***","..-*","...-",".--*","-..-",".--*","--.."};
+ if(Serial.available()>0){
+      ch_hui = Serial.read(); 
+      }else{
+        Serial.print(out);
+      }
+    if(ch_hui==' ' || ch_hui=='\n'){
+      out += " /";
+      }
+else{
+out +=a[ch_hui-97];
+}
+}
