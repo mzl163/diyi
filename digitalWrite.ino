ddc8dcfcd94f347163d120cea4388f2737f30cd6
mzl163 <52455562+mzl163@users.noreply.github.com>
2019-07-04 22:12:56 +0800

    Add files via upload

---
+++ digitalWrite.ino
@@ -0,0 +1,83 @@
+void setup()
+{
+  pinMode(2, OUTPUT);
+   pinMode(3, OUTPUT);
+   pinMode(4, OUTPUT);
+   pinMode(6, OUTPUT);
+  Serial.begin(9600);
+}
+int income;
+void loop()
+{
+  income=Serial.read()-'0';
+  if(income==0)
+  {
+  digitalWrite(2,LOW);
+    digitalWrite(3,LOW);
+    digitalWrite(4,LOW);
+    digitalWrite(6,LOW);
+  }
+  if(income==1)
+  {
+  digitalWrite(2,LOW);
+    digitalWrite(3,LOW);
+    digitalWrite(4,LOW);
+    digitalWrite(6,HIGH);
+  }
+  if(income==2)
+  {
+  digitalWrite(2,LOW);
+    digitalWrite(3,LOW);
+    digitalWrite(4,HIGH);
+    digitalWrite(6,LOW);
+  }
+  if(income==3)
+  {
+  digitalWrite(2,LOW);
+    digitalWrite(3,LOW);
+    digitalWrite(4,HIGH);
+    digitalWrite(6,HIGH);
+  }
+  if(income==4)
+  {
+  digitalWrite(2,LOW);
+    digitalWrite(3,HIGH);
+    digitalWrite(4,LOW);
+    digitalWrite(6,LOW);
+  }
+  if(income==5)
+  {
+  digitalWrite(2,LOW);
+    digitalWrite(3,HIGH);
+    digitalWrite(4,LOW);
+    digitalWrite(6,HIGH);
+  }
+  if(income==6)
+  {
+  digitalWrite(2,LOW);
+    digitalWrite(3,HIGH);
+    digitalWrite(4,HIGH);
+    digitalWrite(6,LOW);
+  }
+  if(income==7)
+  {
+  digitalWrite(2,LOW);
+    digitalWrite(3,HIGH);
+    digitalWrite(4,HIGH);
+    digitalWrite(6,HIGH);
+  }
+  if(income==8)
+  {
+  digitalWrite(2,HIGH);
+    digitalWrite(3,LOW);
+    digitalWrite(4,LOW);
+    digitalWrite(6,LOW);
+  }
+  if(income==9)
+  {
+  digitalWrite(2,HIGH);
+    digitalWrite(3,LOW);
+    digitalWrite(4,LOW);
+    digitalWrite(6,HIGH);
+  }
+}
