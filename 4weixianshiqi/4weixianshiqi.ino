055f31e617e962c81fd790ea8ee2cee8b0ba3758
mzl163 <52455562+mzl163@users.noreply.github.com>
2019-07-05 21:17:28 +0800

    Add files via upload

---
+++ 4weixianshiqi/4weixianshiqi.ino
@@ -0,0 +1,125 @@
+void setup()
+{
+pinMode(10,OUTPUT);
+pinMode(2,OUTPUT);
+pinMode(3,OUTPUT);
+pinMode(4,OUTPUT);
+pinMode(5,OUTPUT);
+pinMode(6,OUTPUT);
+pinMode(7,OUTPUT);
+pinMode(8,OUTPUT);
+Serial.begin(9600);
+}
+
+int b=0;
+int i=0;
+void loop(){
+i=0;
+  while(Serial.available()>0)
+  {
+  b=Serial.read()-'0';
+  open(3-i);
+  light(b);
+    i++;
+    delay(100);
+  }
+}
+void open(int j){
+if(j==0)
+{digitalWrite(5,LOW);
+digitalWrite(6,HIGH);
+digitalWrite(7,HIGH);
+digitalWrite(8,HIGH);
+}
+if(j==1)
+{digitalWrite(5,HIGH);
+digitalWrite(6,LOW);
+digitalWrite(7,HIGH);
+digitalWrite(8,HIGH);
+}
+if(j==2)
+{digitalWrite(5,HIGH);
+digitalWrite(6,HIGH);
+digitalWrite(7,LOW);
+digitalWrite(8,HIGH);
+}
+if(j==3)
+{digitalWrite(5,HIGH);
+digitalWrite(6,HIGH);
+digitalWrite(7,HIGH);
+digitalWrite(8,LOW);
+}
+}
+void light(int income)
+{
+  if(income==0)
+  {
+  digitalWrite(10,LOW);
+      digitalWrite(2,LOW);
+     digitalWrite(3,LOW);
+      digitalWrite(4,LOW);
+  }
+  if(income==1)
+  {
+  digitalWrite(10,LOW);
+   digitalWrite(2,LOW);
+     digitalWrite(3,LOW);
+     digitalWrite(4,HIGH);
+  }
+  if(income==2)
+  {
+  digitalWrite(10,LOW);
+    digitalWrite(2,LOW);
+      digitalWrite(3,HIGH);
+      digitalWrite(4,LOW);
+  }
+  if(income==3)
+  {
+  digitalWrite(10,LOW);
+    digitalWrite(2,LOW);
+      digitalWrite(3,HIGH);
+      digitalWrite(4,HIGH);
+  }
+  if(income==4)
+  {
+  digitalWrite(10,LOW);
+      digitalWrite(2,HIGH);
+      digitalWrite(3,LOW);
+    digitalWrite(4,LOW);
+  }
+  if(income==5)
+  {
+  digitalWrite(10,LOW);
+      digitalWrite(2,HIGH);
+      digitalWrite(3,LOW);
+      digitalWrite(4,HIGH);
+  }
+  if(income==6)
+  {
+  digitalWrite(10,LOW);
+      digitalWrite(2,HIGH);
+      digitalWrite(3,HIGH);
+      digitalWrite(4,LOW);
+  }
+  if(income==7)
+  {
+  digitalWrite(10,LOW);
+      digitalWrite(2,HIGH);
+      digitalWrite(3,HIGH);
+      digitalWrite(4,HIGH);
+  }
+  if(income==8)
+  {
+  digitalWrite(10,HIGH);
+      digitalWrite(2,LOW);
+      digitalWrite(3,LOW);
+      digitalWrite(4,LOW);
+  }
+  if(income==9)
+  {
+  digitalWrite(10,HIGH);
+      digitalWrite(2,LOW);
+      digitalWrite(3,LOW);
+      digitalWrite(4,HIGH);
+  }
+}
